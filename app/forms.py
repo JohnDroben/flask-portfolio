9271b1ff91730fa5d0e553619c71d5b75bd231f6
Drobenuyk George <drobeng68@gmail.com>
2025-06-04 10:10:06 +0300

    Merge branch 'test' of C:\Users\C0500030102001\Documents\GitHub\flask-portfolio with conflicts.

--- app/forms.py
+++ app/forms.py
@@ -7,6 +7,11 @@ from flask_login import current_user
 
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 class RegistrationForm(FlaskForm):
     username = StringField('Имя пользователя',
@@ -17,25 +22,15 @@ class RegistrationForm(FlaskForm):
                              validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Зарегистрироваться')
 
-def validate_username(self, username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Это имя пользователя уже занято')
 
-def validate_email(self, email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Этот email уже используется')
-
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
-
 
 class ProfileForm(FlaskForm):
     username = StringField('Имя пользователя',
